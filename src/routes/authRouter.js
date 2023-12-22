b51058af6a4f77a2ba582713ac89801972b3cfb8
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-22 12:02:05 +0200

    login magic

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { dbQueryWithData } = require('../helper');
 
 const authRouter = express.Router();
 
@@ -7,15 +8,33 @@ const authRouter = express.Router();
 // 400 - user not found
 authRouter.post('/api/auth/login', async (req, res) => {
   // issitraukiam atsiustu duomenis
-
+  const { username, password } = req.body;
   // paieskoti vartotojo duomenu bazej pagal username
+  const sql = 'SELECT * FROM `users` WHERE username=?';
+  const [rows, error] = await dbQueryWithData(sql, [username]);
 
+  console.log('error ===', error);
+  // neradom
+  if (rows.length === 0) {
+    res.status(400).json({
+      msg: 'username not fount (dev)',
+    });
+    return;
+  }
   // radom
-
-  // arr sutampa slaptazodis?
-
-  // jei sutampa sekme
-  res.json('trying login');
+  if (rows.length === 1) {
+    // arr sutampa slaptazodis?
+    if (password !== rows[0].password) {
+      res.status(400).json({
+        msg: 'username or password not match (dev pass no match)',
+      });
+      return;
+    }
+    // pass match
+    res.json({
+      msg: 'loging success',
+    });
+  }
 });
 
 module.exports = authRouter;
