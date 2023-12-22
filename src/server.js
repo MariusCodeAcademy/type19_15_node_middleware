754bdeae036c6f1ae7c024a91fe99d93cb562645
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-22 09:38:58 +0200

    our middle ware

--- src/server.js
+++ src/server.js
@@ -4,21 +4,48 @@ const morgan = require('morgan');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
 const { dbConfig } = require('./config');
+const { dbQueryWithData } = require('./helper');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+// my own middle ware
+const logHello = (req, res, next) => {
+  console.log('--- Hi there -- Im -- middleware!!!---');
+  // leidzia kodui vykti toliau
+  next();
+};
+const reqTime = (req, res, next) => {
+  const now = new Date();
+  const time = now.toTimeString();
+  console.log('request:', time);
+  // leidzia kodui vykti toliau
+  next();
+};
+
+// Middleware - app level
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
+app.use(logHello);
+// app.use(reqTime);
 
 app.get('/', (req, res) => {
   res.json('Hello World');
 });
 
-testConnection();
+// get all posts      route level middleware
+app.get('/api/posts', reqTime, async (req, res) => {
+  const sql = 'SELECT * FROM posts';
+  const [rows, error] = await dbQueryWithData(sql);
+
+  console.log('error ===', error);
+
+  res.json(rows);
+});
+
+// testConnection();
 // connect
 async function testConnection() {
   let conn;
