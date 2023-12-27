bb8f08060bfa1d09f8088b2cfd7a9588fdc030db
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-27 10:54:23 +0200

    add post uzd

---
+++ front-end/js/add.js
@@ -0,0 +1,10 @@
+'use strict';
+console.log('add.js file was loaded');
+
+// nusitaikyti i forma ir inputus
+// surinkiti inputus i objeka ir isiusti ji i backend
+// jei sekmingai sukurtas postas, naviguoti i index.html
+
+// backe yra vaildacija title ir autoriui
+// A pranesti bendru pranesimu (sukurti/atslepti alert elementa)
+// B atvaizduoti lauka kuriame yra klaida su klaidos klase (is-invalid klase)
